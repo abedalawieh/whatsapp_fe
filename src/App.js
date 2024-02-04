a61f3f6bb16e76c0c86926bebe65553b9846ccb5
abedalawieh <abedalaweye@gmail.com>
2024-02-04 14:04:16 +0200

    Testing the tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ChatIcon } from "./svg";
 function App() {
   return (
     <div>
-      <h1 className="text-red-500">welcome</h1>
+      <h1 className="text-red-950 font-bold bg-black">Welcome</h1>{" "}
     </div>
   );
 }
