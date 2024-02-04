5be061aa4f5d4e4d3ee6069b32d2c943106b5bae
abedalawieh <abedalaweye@gmail.com>
2024-02-04 14:22:55 +0200

    Adding tailwind css variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { ChatIcon } from "./svg";
 function App() {
-  return (
-    <div>
-      <h1 className="text-red-950 font-bold bg-black">Welcome</h1>{" "}
-    </div>
-  );
+  return <div className="dark"></div>;
 }
 
 export default App;
